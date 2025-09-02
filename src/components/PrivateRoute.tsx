c914356edf3dad079c20e216aec784a3e4edb36a
MindsMap-intern <stdk87267@gmail.com>
2025-09-02 11:49:46 +0530

    damn

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
-export default function PrivateRoute({ children }: { children: JSX.Element }) {
+export default function PrivateRoute({ children }: { children: React.ReactNode }) {
   const { user, loading } = useAuth();
   if (loading) return <div />;
   return user ? children : <Navigate to="/login" replace />;
